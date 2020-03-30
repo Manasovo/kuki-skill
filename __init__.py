37605f015c520154e181f01b6ff5d61c907e745a
Marek Bukal <mara@moon.local>
2020-03-30 22:24:56 +0200

    testing hello world

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
+from mycroft.skills.common_play_skill import CommonPlaySkill
+
 from enum import Enum
 
 class DeviceType(Enum):
