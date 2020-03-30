6f3eaabb198b23ce6351851075537e14d0d6b4dd
Marek Bukal <mara@moon.local>
2020-03-30 21:19:05 +0200

    testing hellow world

--- __init__.py
+++ __init__.py
@@ -11,10 +11,24 @@ class KukiSkill(MycroftSkill):
         my_setting = self.settings.get('my_setting')
 
   
-   @intent_handler(IntentBuilder('')
+    @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
+    def handle_thank_you_intent(self, message):
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("welcome")
+
+    @intent_handler('HowAreYou.intent')
+    def handle_how_are_you_intent(self, message):
+        """ This is a Padatious intent handler.
+        It is triggered using a list of sample phrases."""
+        self.speak_dialog("how.are.you")
+
+    @intent_handler(IntentBuilder('')
                     .require('HelloWorldKeyword'))
     def handle_hello_world_intent(self, message):
- 
+        """ Skills can log useful information. These will appear in the CLI and
+        the skills.log file."""
+        self.log.info("There are five types of log messages: "
+                      "info, debug, warning, error, and exception.")
         self.speak_dialog("hello.world")
 
   
