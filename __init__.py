b3ecddbc8683363b5553ff82f164fedf6d7d20be
Marek Bukal <mara@moon.local>
2020-03-30 22:12:07 +0200

    testing hello world

--- __init__.py
+++ __init__.py
@@ -1,48 +1,6 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
-class DeviceType(Enum):
-    DEFAULT = 1
-    FIRSTBEST = 2
-    NOTFOUND = 3
-
-
-class KukiSkill(CommonPlaySkill):
-    """Kuki control through the Kuki API."""
-
-    def __init__(self):
-        super(KukiSkill, self).__init__()
-        self.index = 0
-        self.kuki = None
-        self.process = None
-        self.device_name = None
-      
-
-def get_default_device(self):
-        """Get preferred playback device."""
-       
-            # No playing device found, use the default Kuki device
-            default_device = self.settings.get('default_device', '')
-            dev = None
-            device_type = DeviceType.NOTFOUND
-            if default_device:
-                dev = self.device_by_name(default_device)
-                self.is_player_remote = True
-                device_type = DeviceType.DEFAULT
-              # use first best device if none of the prioritized works
-            if not dev and len(self.devices) > 0:
-                dev = self.devices[0]
-                self.is_player_remote = True  # ?? Guessing it is remote
-                device_type = DeviceType.FIRSTBEST
-
-            if dev and not dev['is_active']:
-                self.kuki.transfer_playback(dev['id'], False)
-            self.log.info('Device detected: {}'.format(device_type))
-            return dev
-
-        return None
-    
-
 class KukiSkill(MycroftSkill):
     def __init__(self):
         super().__init__()
@@ -56,9 +14,6 @@ class KukiSkill(MycroftSkill):
                     .require('play'))
     def handle_hello_world_intent(self, message):
         self.speak_dialog("play")
-
-
-
 
 
 
