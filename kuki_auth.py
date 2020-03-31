5e09ad5fd5ff83ae4df9c491a1230ac1b7353f98
Marek Bukal <mara@moon.local>
2020-03-31 21:01:26 +0200

    testing hello world

--- kuki_auth.py
+++ kuki_auth.py
@@ -34,6 +34,25 @@ def get_token(dev_cred):
 
 
 
+class MycroftKukiAuth(KukiClientAuth):
+    """ Credentials object renewing through the Mycroft backend."""
+    def __init__(self, dev_cred):
+        self.dev_cred = dev_cred
+        self.access_token = None
+        self.expiration_time = None
+        self.get_access_token()
+
+    def get_access_token(self, force=False):
+        if (not self.access_token or time.time() > self.expiration_time or
+                force):
+            d = get_token(self.dev_cred)
+            self.access_token = d['access_token']
+            # get expiration time from message, if missing assume 1 hour
+            self.expiration_time = d.get('expiration') or time.time() + 3600
+        return self.access_token
+
+
+
 
 
 def GenerateSerial(StringLength=56):
