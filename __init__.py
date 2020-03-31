4a6b846ee9937a3d8b2ce0be372be7b29dfbe111
Marek Bukal <mara@moon.local>
2020-03-31 20:33:51 +0200

    testing hello world

--- __init__.py
+++ __init__.py
@@ -26,11 +26,40 @@ class KukiSkill(MycroftSkill):
 class KukiSkill(MycroftSkill):
     """Kuki control through the Kuki API."""
 
-     @intent_handler(IntentBuilder('').require('Kuki').require('Device'))
-        def list_devices(self, message):
+
+    def devices(self):
+        """Devices, cached for 60 seconds."""
+        if not self.kuki:
+            return []  # No connection, no devices
+        now = time.time()
+        if not self.__device_list or (now - self.__devices_fetched > 60):
+            self.__device_list = self.kuki.get_devices()
+            self.__devices_fetched = now
+        return self.__device_list
+
+    def device_by_name(self, name):
+        """Get a kuki devices from the API.
+
+        Arguments:
+            name (str): The device name (fuzzy matches)
+        Returns:
+            (dict) None or the matching device's description
+        """
+        devices = self.devices
+        if devices and len(devices) > 0:
+            # Otherwise get a device with the selected name
+            devices_by_name = {d['name']: d for d in devices}
+            key, confidence = match_one(name, list(devices_by_name.keys()))
+            if confidence > 0.5:
+                return devices_by_name[key]
+        return None
+
+
+    @intent_handler(IntentBuilder('').require('Kuki').require('Device'))
+    def list_devices(self, message):
         """ List available devices. """
-        if self.spotify:
-            devices = [d['name'] for d in self.spotify.get_devices()]
+        if self.kuki:
+            devices = [d['name'] for d in self.kuki.get_devices()]
             if len(devices) == 1:
                 self.speak(devices[0])
             elif len(devices) > 1:
