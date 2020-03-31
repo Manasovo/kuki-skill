9e4cf2ef6037b97743155bbdb036ca047b8e9532
Marek Bukal <mara@moon.local>
2020-03-31 21:04:18 +0200

    testing hello world

--- kuki_auth.py
+++ kuki_auth.py
@@ -10,7 +10,7 @@ import string                         # generate serial
 
 from mycroft.api import DeviceApi     # testing
 from mycroft.util.log import LOG      # testing
-
+import time                           # testing
 
 
 def get_token(dev_cred):
