d23d28c9131b703dc20a7bbc81386cb053a139a8
Marek Bukal <mara@loom.dvl>
2020-04-01 14:39:45 +0200

    testing hello world

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ class KukiSkill(MycroftSkill):
     def list_devices(self, message):
         """ List available devices. """
         
-        TEST = KukiConnect.get_session()
+        TEST = KukiConnect.get_session(self)
 
         #KukiConnect.get_devices()
         
