8fcf25981c8263e6a1aeb3221b8a6f6b3f3b7d52
Marek Bukal <mara@loom.dvl>
2020-04-01 14:01:53 +0200

    testing hello world

--- __init__.py
+++ __init__.py
@@ -47,6 +47,7 @@ class KukiSkill(MycroftSkill):
             else:
                 self.speak_dialog('NoDevicesAvailable')
         else:
+            self.log.info('PRDEL')
             self.log.error("Kuki AUTH failed")
             # self.failed_auth()
 
