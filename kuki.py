ce6725ba630997d89214e7b700660acf43f47d09
Marek Bukal <mara@loom.dvl>
2020-04-01 15:18:45 +0200

    testing hello world

--- kuki.py
+++ kuki.py
@@ -1,3 +1,26 @@
+""" A simple Python library for the Kuki Web API """
+
+# importing the requests library 
+import requests                       # http post & get
+import json                           # json :-)
+import uuid                           # mac
+import socket                         # hostname
+import random                         # generate serial
+import string                         # generate serial
+
+from mycroft.api import DeviceApi     # testing
+from mycroft.util.log import LOG      # testing
+import time                           # testing
+
+# defining the api-endpoint
+self.log.error("DEBUG1")
+API_URL = "https://as.kukacka.netbox.cz/api-v2/"
+
+
+#class KukiConnect(object):
+#      """ Implement the Kuki Connect API """
+#    self.log.error("DEBUG2")
+
 def GenerateSerial(StringLength=56):
         """Generate a random string of letters and digits """
 
