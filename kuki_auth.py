9f5aa968860c9dffe719086af6e7c62c806209a6
Marek Bukal <mara@loom.dvl>
2020-04-01 11:33:23 +0200

    testing hello world

--- kuki_auth.py
+++ kuki_auth.py
@@ -1,19 +1,66 @@
-""" A simple Python library for the Kuki Web API """
+mport spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from requests import HTTPError
+import time
 
-# importing the requests library 
-import requests                       # http post & get
-import json                           # json :-)
-import uuid                           # mac
-import socket                         # hostname
-import random                         # generate serial
-import string                         # generate serial
-
-from mycroft.api import DeviceApi     # testing
-from mycroft.util.log import LOG      # testing
-import time                           # testing
+from mycroft.api import DeviceApi
+from mycroft.util.log import LOG
 
 def get_token(dev_cred):
+    """ Get token with a single retry.
+    Args:
+        dev_cred: OAuth Credentials to fetch
+     """
+    retry = False
+    try:
+        d = DeviceApi().get_oauth_token(dev_cred)
+    except HTTPError as e:
+        if e.response.status_code == 404:  # Token doesn't exist
+            raise
+        if e.response.status_code == 401:  # Device isn't paired
+            raise
+        else:
+            retry = True
+    if retry:
+        d = DeviceApi().get_oauth_token(dev_cred)
+    return d
 
-class MycroftKukiAuth(object):
 
-class KukiConnect(MycroftKukiAuth):
+class MycroftKukiCredentials(SpotifyClientCredentials):
+    """ Credentials object renewing through the Mycroft backend."""
+    def __init__(self, dev_cred):
+        self.dev_cred = dev_cred
+        self.access_token = None
+        self.expiration_time = None
+        self.get_access_token()
+
+    def get_access_token(self, force=False):
+        if (not self.access_token or time.time() > self.expiration_time or
+                force):
+            d = get_token(self.dev_cred)
+            self.access_token = d['access_token']
+            # get expiration time from message, if missing assume 1 hour
+            self.expiration_time = d.get('expiration') or time.time() + 3600
+        return self.access_token
+
+
+def refresh_auth(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except HTTPError as e:
+            if e.response.status_code == 401:
+                self.client_credentials_manager.get_access_token(force=True)
+                return func(self, *args, **kwargs)
+            else:
+                raise
+    return wrapper
+
+
+class KukiConnect(spotipy.Spotify):
+    """ Implement the Spotify Connect API.
+    See:  https://developer.spotify.com/web-api/
+
+    This class extends the spotipy.Spotify class with Spotify Connect
+    methods since the Spotipy module including these isn't released yet.
+    """
