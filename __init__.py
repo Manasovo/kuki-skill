8da4e09fb7cf353e294330186cf2e7eac88ed340
Marek Bukal <mara@loom.dvl>
2020-04-02 13:22:01 +0200

    list devices

--- __init__.py
+++ __init__.py
@@ -147,7 +147,7 @@ class KukiSkill(MycroftSkill):
 
 
     # status of device
-    @intent_handler(IntentBuilder('').require('Status'))
+    @intent_handler(IntentBuilder('').require('Status').require('Kuki').require('Device'))
     def status_intent(self, message):
         self.api_headers = {'X-SessionKey': kuki_session(self)}
         self.api_get = requests.get(API_URL + 'device', headers = self.api_headers)
