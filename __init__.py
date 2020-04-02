e65c20341e90f59ec64c05a0ed1ed47196ec32f1
Marek Bukal <mara@loom.dvl>
2020-04-02 16:00:54 +0200

    list devices state

--- __init__.py
+++ __init__.py
@@ -235,9 +235,9 @@ class KukiSkill(MycroftSkill):
 
 
         # sending post request and saving response as response object
-        self.api_response = requests.post(url = API_REMOTE_URL + DEVICE_ID, headers = self.api_headers, data = self.api_post)
+        #self.api_response = requests.post(url = API_REMOTE_URL + DEVICE_ID, headers = self.api_headers, data = self.api_post)
         
-        #self.api_response = requests.post('https://as.kukacka.netbox.cz/api/remote/30928.json?X-SessionKey=52a0011b-8f52-4a43-8580-240f7d198718', data = self.api_post')
+        self.api_response = requests.post('https://as.kukacka.netbox.cz/api/remote/30928?X-SessionKey=52a0011b-8f52-4a43-8580-240f7d198718', data = self.api_post')
 
         self.log.error(self.api_response)
 
