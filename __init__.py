bde7402bf42ae274cbbda50292005ea892182da0
Marek Bukal <mara@moon.local>
2020-04-02 21:21:46 +0200

    Kuki remote control

--- __init__.py
+++ __init__.py
@@ -191,24 +191,8 @@ class KukiSkill(MycroftSkill):
         self.log.error("DEBUG STATUS")
 
         kuki_session(self)
-        devices = kuki_devices(self)
-        self.log.debug(devices)
-        
-        # API get - TODO set prefered devices
-        self.api_headers = {'X-SessionKey': session}  
-
-        self.api_get = requests.get(API_URL + 'device', headers = self.api_headers)
-        self.result = json.loads(self.api_get.text)
-        
-        # POKUSY O DODANI ID
-        # self.prefered_device_id = self.result[0]['id']
-        # self.prefered_device_id = ([result_item['id'] for result_item in self.result])
-        
-        self.log.error("==============")
-        self.log.error(prefered_device)
-        self.prefered_device_id = list(map(lambda item: item['id'], filter(lambda item: item['alias'] == 'prefered_device' self.result)))
-        
-        #self.prefered_device_id = "5034042" #ostra
+   
+        self.prefered_device_id = "5034042" #ostra
         #self.prefered_device_id = "30928" #testovka
         
         self.log.error(self.prefered_device_id)
