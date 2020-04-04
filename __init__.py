fc3390c74e1f11382544e073a5c0599f7c4655ef
Marek Bukal <mara@moon.local>
2020-04-04 19:05:40 +0200

    generate persistant serial number

--- __init__.py
+++ __init__.py
@@ -73,7 +73,8 @@ def generate_serial(StringLength=56):
     serial_number =  "kuki2.0_" + ''.join(random.choice(LettersAndDigits) for i in range(StringLength))
 
     #save_data_file(filename=['kuki_skill_serial.cfg'], data=['serial_number'])
-    save_data_files(filename(kuki_skill_serial.cfg, data(serial_number))
+    save_data_files(filename(kuki_skill_serial.cfg, data(serial_number)))
+
 
         #  self.start_playlist_playback(dev, data['name'],
         #                                     data['data'])
